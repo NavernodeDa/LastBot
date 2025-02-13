a6c0b8b676bcaeace1bd2809984d38cbb7dd7a7c
Kumcha <kumha@duck.com>
2025-02-13 21:22:42 +0300

    Adding runBlocking for main()

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,20 +7,31 @@ import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.serialization.gson.*
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
 import org.slf4j.LoggerFactory
 
-fun main() {
-    val config = ConfigurationProperties.fromResource("config.properties")
-    startUpdate(
-        config,
-        LoggerFactory.getLogger("SpotifyBotLogger"),
-        HttpClient(CIO) {
-            install(ContentNegotiation) {
-                gson()
-            }
-            install(UserAgent) {
-                config[Data.userAgent]
-            }
-        },
-    )
-}
+fun main(): Unit =
+    runBlocking {
+        val config = ConfigurationProperties.fromResource("config.properties")
+        val logger = LoggerFactory.getLogger("SpotifyBotLogger")
+        try {
+            val n =
+                async {
+                    startUpdate(
+                        config,
+                        logger,
+                        HttpClient(CIO) {
+                            install(ContentNegotiation) {
+                                gson()
+                            }
+                            install(UserAgent) {
+                                config[Data.userAgent]
+                            }
+                        },
+                    )
+                }
+        } catch (e: Exception) {
+            logger.error("An error occurred: ", e)
+        }
+    }
